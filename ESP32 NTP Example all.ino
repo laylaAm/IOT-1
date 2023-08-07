97d7c723e209fea8d1ecc3d5fd70acdadf1878c3
laylaAm <139586277+laylaAm@users.noreply.github.com>
2023-08-07 20:13:40 +0300

    Add files via upload

--- ESP32 NTP Example all.ino
+++ ESP32 NTP Example all.ino
@@ -4,25 +4,22 @@
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 
-const String forwardUrl = "https://s-m.com.sa/f.html";
-const String backwardUrl = "https://s-m.com.sa/b.html";
-const String rightUrl = "https://s-m.com.sa/r.html";
-const String leftUrl = "https://s-m.com.sa/l.html";
+const String url = "https://s-m.com.sa/f.html";
 String payload = "";
 
 // Define pins
 const int ledPin1 = 25;
 const int ledPin2 = 5;
-const int ledPin3 = 32;
-const int ledPin4 = 33;
+const int ledPin6 = 32;
+const int ledPin7 = 33;
 
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
-  pinMode(ledPin3, OUTPUT);
-  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin6, OUTPUT);
+  pinMode(ledPin7, OUTPUT);
 
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
@@ -33,12 +30,12 @@ void setup() {
   Serial.print("OK! IP=");
   Serial.println(WiFi.localIP());
 
-  Serial.print("Fetching " + forwardUrl + backwardUrl+ rightUrl+ leftUrl +"... ");
+  Serial.print("Fetching " + url + "... ");
 }
 
 void loop() {
   HTTPClient http;
-  http.begin(forwardUrl);
+  http.begin(url);
   int httpResponseCode = http.GET();
 
   if (httpResponseCode > 0) {
@@ -48,19 +45,16 @@ void loop() {
     Serial.println();
     Serial.println(payload);
 
-    if (payload == "forward ") {
-      digitalWrite(ledPin1, HIGH);
-    } else if (payload == "Right") {
-      digitalWrite(ledPin2, HIGH);
-    } else if (payload == "Backward") {
-      digitalWrite(ledPin3, HIGH);
-    } else if (payload == "left") {
-      digitalWrite(ledPin4, HIGH);
+    if (payload == "forward") {
+      digitalWrite(ledPin1, HIGH); // Turn on LED 1
+      digitalWrite(ledPin2, HIGH); // Turn on LED 2
+      digitalWrite(ledPin6, HIGH); // Turn on LED 3
+      digitalWrite(ledPin7, HIGH); // Turn on LED 4
     } else {
       digitalWrite(ledPin1, LOW); // Turn off all LEDs if unknown response received
       digitalWrite(ledPin2, LOW);
-      digitalWrite(ledPin3, LOW);
-      digitalWrite(ledPin4, LOW);
+      digitalWrite(ledPin6, LOW);
+      digitalWrite(ledPin7, LOW);
     }
   } else {
     Serial.print("Error code: ");
